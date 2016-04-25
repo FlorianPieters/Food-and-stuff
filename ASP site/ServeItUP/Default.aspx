6d0ab73f55317dc1d0b4502bfc64f46dc89d06e7
FlorianPieters <florian.pieters@hotmail.com>
2016-04-25 11:30:28 +0200

    FB

--- ASP site/ServeItUP/Default.aspx
+++ ASP site/ServeItUP/Default.aspx
@@ -4,13 +4,74 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>facebook</title>
+    <meta charset="UTF-8">
 </head>
 <body>
-    <form id="form1" runat="server">
-    <div>
+
+<script>
+ 
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+  
+    if (response.status === 'connected') {
     
-    </div>
-    </form>
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    }
+    else {
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+  }
+
+ 
+  function checkLoginState() {
+      FB.getLoginStatus(function(response) {
+          statusChangeCallback(response);
+      });
+  }
+
+  window.fbAsyncInit = function() {
+      FB.init({
+          appId: '{255539311458984}',
+          cookie     : true,  
+          xfbml      : true, 
+          version    : 'v2.5' 
+      });
+
+ FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+</script>
+
+    <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+</fb:login-button>
+
+<div id="status">
+</div>
+
 </body>
 </html>
--- ASP site/ServeItUP/Default.aspx.cs
+++ ASP site/ServeItUP/Default.aspx.cs
@@ -1,14 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Facebook;
+
+
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+       // CheckAutorization();
+        
     }
+  /*  private void CheckAutorization()
+    {
+        string app_id = "255539311458984";
+        string app_secret = "b6a76e18e91420fbd81a54c78cb893a0";
+        string scope = "publish_stream, manage_page";
+
+        if (Request["code"] == null)
+        {
+            Response.Redirect(string.Format(
+                "https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&scope={2}",
+                app_id, Request.Url.AbsoluteUri, scope));
+        }
+        else
+        {
+            Dictionary<string, string> tokens = new Dictionary<string, string>();
+
+            string url = string.Format("https://graph.facebook.com/oauth/acces_token?client_id={0}&redirect_uri={1}&scope={2}&code={3}&client_secret={4}",
+                app_id, Request.Url.AbsoluteUri, scope, Request["code"].ToString(), app_secret);
+
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                string vals = reader.ReadToEnd();
+
+                foreach(string token in vals.Split('&'))
+                {
+                    tokens.Add(token.Substring(0, token.IndexOf("=")),
+                       token.Substring(token.IndexOf("=") + 1, token.Length - token.IndexOf("=") - 1));
+                }
+
+                string access_token = tokens["acces_token"];
+
+                var client = new FacebookClient(access_token);
+
+              //  client.Post("/me/feed", new { message = "markhagan.me video tutorial" });
+
+
+            }
+                
+        }
+    } */
 }
